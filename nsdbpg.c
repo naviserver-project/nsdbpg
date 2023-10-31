d0a40ac4b855d7b6f84df3e6e0ef7bac21013208
Gustaf Neumann <neumann@wu-wien.ac.at>
2023-10-31 10:09:49 +0100

    Fixed a potential memory leak in UTF-8 conversions
    A potential memory leak was introduced with the back and forth
    conversion between "external" UTF8 and Tcl's internal UTF8
    variant. The problem shows up when returning multiple tuples, where a
    string value larger than TCL_DSTRING_STATIC_SIZE is returned (unless,
    this comes from the last column). The problem was induced by an
    irregularity passing Tcl_DStrings to functions. The function
    Tcl_ExternalToUtfDString() blindly initializes the passed-in pointer
    to a Tcl_DString, which will lead to a memory leak, if this
    Tcl_DString has already a pre-allocated size.  So, e.g., the usual
    idiom setting the length of the dsPtr to 0 to keep the pre-allocated
    dynamic memory causes a leak.

--- nsdbpg.c
+++ nsdbpg.c
@@ -584,25 +584,31 @@ GetRow(const Ns_DbHandle *handle, Ns_Set *row)
 
         } else {
             size_t i;
-            Tcl_DString ds, *dsPtr = &ds;
 
-            Tcl_DStringInit(dsPtr);
             Ns_SetClearValues(row, 4096);
 
             for (i = 0u; i < (size_t)pconn->nCols; i++) {
+                Tcl_DString ds, *dsPtr = &ds;
                 int   rawFieldLength = PQgetlength(pconn->res, pconn->curTuple, (int)i);
                 char *rawFieldValue  = PQgetvalue(pconn->res, pconn->curTuple, (int)i);
 
                 /*Ns_Log(Notice, "nsdbpg(%s): GetRow %lu '%s' PQgetlength %d",
                   handle->poolname, i,  rawFieldValue, rawFieldLength);*/
+                Ns_Log(Notice, "nsdbpg(%s): GetRow %lu  PQgetlength %d",
+                       handle->poolname, i,  rawFieldLength);
+                /*
+                 * Tcl_ExternalToUtfDString performs by itself a
+                 * Tcl_DStringInit(dsPtr); passing a dsPtr with pre-allocated
+                 * size causes a memory leak (e.g., when using the usual idiom
+                 * setting the length of the dsPtr to 0 to keep the
+                 * pre-allocated dynamic memory).
+                 */
                 (void)Tcl_ExternalToUtfDString(NULL, rawFieldValue, (TCL_SIZE_T)rawFieldLength, dsPtr);
                 /*Ns_Log(Notice, "nsdbpg(%s): GetRow %lu converted '%s' PQgetlength %d",
                   handle->poolname, i, dsPtr->string, dsPtr->length);*/
                 Ns_SetPutValueSz(row, i, dsPtr->string, dsPtr->length);
-
-                Ns_DStringSetLength(dsPtr, 0);
+                Tcl_DStringFree(dsPtr);
             }
-            Tcl_DStringFree(dsPtr);
 
 #ifdef NS_SET_DEBUG
             Ns_Log(Notice, "nsdbpg(%s): GetRow set %p size %lu maxsize %lu data len %d avail %d DONE",
